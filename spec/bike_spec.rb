7271d9f9df3147f344b7105be1d4fb236408480e
Rhys Stansfield <rhys@onlinesalesgrowth.com>
2013-10-15 11:23:56 +0100

    Add fix! method

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,14 +2,21 @@ require 'bike'
 
 describe Bike do
 
+  let(:bike) { Bike.new }
+
   it 'should be working when first rented' do
-    bike = Bike.new
     expect(bike).not_to be_broken
   end
 
   it "can be broken" do 
-    bike = Bike.new
     bike.break!
     expect(bike).to be_broken
   end
+
+  it 'can be fixed' do
+    bike.break!
+    bike.fix!
+    expect(bike).not_to be_broken
+  end
+
 end
